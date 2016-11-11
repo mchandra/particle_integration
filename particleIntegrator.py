37f53119e21e6e74644c848e67acd1ff84711224
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2016-11-11 15:39:16 +0530

    Addition of normalized density

--- particleIntegrator.py
+++ particleIntegrator.py
@@ -1,15 +1,10 @@
 import numpy as np
-import scipy 
-import random
 import pylab as pl
 from scipy.integrate import odeint
-import scipy.integrate as integ
 import scipy.stats as stats
-import matplotlib.patches as patches
-from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import plotly.plotly as py
+
+
 
 #%%
 """ Set plot parameters to make beautiful plots """
@@ -40,8 +35,7 @@ pl.rcParams['ytick.labelsize']  = 'medium'
 pl.rcParams['ytick.direction']  = 'in'
 """ Setting number of particles"""
 
-no_of_particles = 5000
-
+no_of_particles = int(input("Enter number of particles:"))
 #%%
 
 """ function definition """
@@ -100,14 +94,16 @@ initial_conditions = np.concatenate([initial_conditions_position_x,\
 
 #%%
 """ Discretizing Space """
-
-dx = 1
+x_divisions=int(input("Enter the number of divisions along x:"))
+dx = (length_of_box_x/x_divisions)
 x = np.arange(left_boundary,right_boundary,dx)
 x= np.concatenate((x,[right_boundary]),axis = 0)
 
-dy = 1
+y_divisions=int(input("Enter the number of divisions along y:"))
+dy = (length_of_box_y/y_divisions)
 y = np.arange(bottom_boundary,top_boundary,dy)
 y = np.concatenate((y,[top_boundary]), axis =0)
+
 
 #%%
 """ Discretizing time and making sure scaling is done right """
@@ -120,158 +116,62 @@ a = np.zeros((4,6), dtype =np.float)
 
 
 #%%
-
-""" Initialization of solution matrix for all particles """
-
-
-solution_all = np.zeros((4*no_of_particles,time.size),dtype=np.float)
-
-#%%
-
-""" Solving """
-BC=input("1 for Hard and 2 for Periodic")
-
-for time_index,t0 in enumerate(time):
-    
-    t0 = time[time_index] 
-    
-    if(time_index==time.size-1):
-        break
-    t1 = time[time_index+1]
-    t = [t0, t1] 
-    if(time_index==0):
-        initial_conditions = initial_conditions
-        print('started')
-    else:
-        initial_conditions = (solution_all[:,time_index-1])
-    sol = odeint(dY_dt,initial_conditions,t)
-    solution_all[:,time_index] = sol[1,:]
-    if(BC=='1'):
-    
-        for i in range(2*no_of_particles):
-            if(solution_all[i,time_index]>right_boundary):
-                solution_all[2*no_of_particles+i,time_index] = solution_all[2*no_of_particles+i,time_index] * (-1)
-            if(solution_all[i,time_index]<left_boundary):
-                solution_all[2*no_of_particles+i,time_index] = solution_all[2*no_of_particles+i,time_index] * (-1)
-                
-    if(BC=='2'):
-        for i in range(2*no_of_particles):
-            if(solution_all[i,time_index]>right_boundary):
-                solution_all[i,time_index] = solution_all[i,time_index] - length_of_box_x
-            if(solution_all[i,time_index]<left_boundary):
-                solution_all[i,time_index] = solution_all[i,time_index] + length_of_box_x
-                
-
-
-                
-
-             
-            
-#%%
 """ Initializing density matrix """
 
-density = np.zeros((x.size-1,y.size-1,time.size),dtype=np.float)
-
-
-
+density = np.zeros((x.size-1,y.size-1),dtype=np.float)
 
 #%%
-""" Computing density plots """
+
+"""Computing the density matrix for the initial conditions"""
+
 i=0
 j=0
 x_zone = 0
 y_zone = 0
 
 
-for time_index,t0 in enumerate(time):
-    for p in range(0,no_of_particles):
-        for i in range(0,x.size-1):
-            if((solution_all[p,time_index]>x[i])and(solution_all[p,time_index]<x[i+1])):
-                x_zone = i
-        for j in range(0,y.size-1):
-            if((solution_all[p+no_of_particles,time_index]>y[j])and(solution_all[p+no_of_particles,time_index]<y[j+1])):
-                y_zone = j
-        
-        density[x_zone,y_zone,time_index] = density[x_zone,y_zone,time_index] +1
-       
-        
-       
-for time_index,t0 in enumerate(time):
-    density[:,:,time_index]= density[:,:,time_index].transpose()
-        
-        
-for time_index,t0 in enumerate(time):
-    for i in range(0,y.size-1):
-        temp_array=density[:,i,time_index]
-        density[:,i,time_index] = temp_array[::-1]
 
+for p in range(0,no_of_particles):
+    for i in range(0,x.size-1):
+        if((initial_conditions[p]>x[i])and(initial_conditions[p]<x[i+1])):
+            x_zone = i
+    for j in range(0,y.size-1):
+        if((initial_conditions[p+no_of_particles]>y[j])and(initial_conditions[p+no_of_particles]<y[j+1])):
+            y_zone = j
+    
+    density[x_zone,y_zone] = density[x_zone,y_zone] +1
+       
+        
+       
+density=density.transpose()
+        
+        
+for i in range(0,y.size-1):
+    temp_array=density[:,i]
+    density[:,i] = temp_array[::-1]
 
 #%%
+""" normalizing the density """
+density = density/(no_of_particles/(x_divisions*y_divisions))
+
+""" sin perturbation """
+for i in range(0,x.size-1):
+    density[:,i] = density[:,i] + 0.5*np.sin((2*i*np.pi)/x_divisions)
+
+
 
 """ plotting the density contour plot"""
 X, Y = np.meshgrid(x,y)
 
-density_at_timeindex = density[:,:,0]
-pl.contourf(density[:,:,0],100)
+
+pl.contourf(density,100)
 pl.colorbar()
-pl.show()
 #pl.savefig('/media/tejas/games/quazarWork/nParticleDensity/contour/point_mass' + '%04d'%time_index + '.png')
-pl.clf()
+#pl.savefig('/media/tejas/games/quazarWork/nParticleDensity/InitialContour/IC'+'%04d'%x_divisions  + 'divisions.png')
+pl.show()
+pl.figure()
+#pl.clf()
 
-#%%
-""" Making images of the particles """
+pl.plot(density[(y_divisions/2),:])
+pl.show()
 
-
-for time_index,t0 in enumerate(time[::10]):
-    for i in range(0,no_of_particles):
-        
-        plt.plot(solution_all[i][time_index],solution_all[no_of_particles + i][time_index], 'o',color='blue', markersize=10, alpha = 0.4)
-        plt.axhline(y=-1)
-        plt.axvline(x=-1)
-        plt.axhline(y=1)
-        plt.axvline(x=1)
-        plt.xlim([left_boundary, right_boundary])
-        plt.ylim([bottom_boundary, top_boundary])
-        plt.title('$\mathrm{Time}$ = ' + str(time[time_index]) )
-        plt.xlabel('$x$')
-        plt.ylabel('$y$')
-    print ("Time index = ", time_index)
-    plt.savefig('/media/tejas/games/quazarWork/nParticleDensity/images/point_mass' + '%04d'%time_index + '.png')
-    plt.clf()
-   
-    
-#%%
-
-""" plotting maxwellian distribution curves """
-
-
-
-
-params = maxwell.fit(abs(initial_conditions_velocity_x), floc=0)
-
-
-plt.hist(abs(initial_conditions_velocity_x), bins=30, normed=True)
-initialConditionsVelocityX = np.linspace(0, 6, 100)
-plt.plot(initialConditionsVelocityX, maxwell.pdf(initialConditionsVelocityX, *params), lw=3)
-plt.show()
-#plt.savefig('/media/tejas/games/quazarWork/nParticleDensity/velocity_x_PDC')
-plt.clf()
-
-maxwell = stats.maxwell
-
-
-params = maxwell.fit(abs(initial_conditions_velocity_y), floc=0)
-
-
-plt.hist(abs(initial_conditions_velocity_y), bins=30, normed=True)
-initialConditionsVelocityY = np.linspace(0, 6, 100)
-plt.plot(initialConditionsVelocityY, maxwell.pdf(initialConditionsVelocityY, *params), lw=3)
-plt.show()
-#plt.savefig('/media/tejas/games/quazarWork/nParticleDensity/velocity_y_PDC')
-plt.clf()
-
-
-
-
-
-#%%
