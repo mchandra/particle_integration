7b9b13edc8201a2d21ef5a411c6c03eb4dcddca3
TejasMane <ezio.tejas@gmail.com>
2016-11-11 18:46:34 +0530

    Update plotError.py

--- plotError.py
+++ plotError.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pylab as pl
-from scipy.integrate import odeint
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import math
 
+#%%
+
+%matplotlib inline
 
 """ Set plot parameters to make beautiful plots """
 pl.rcParams['figure.figsize']  = 12, 7.5
@@ -24,7 +23,6 @@ pl.rcParams['xtick.minor.pad']  = 8
 pl.rcParams['xtick.color']      = 'k'
 pl.rcParams['xtick.labelsize']  = 'medium'
 pl.rcParams['xtick.direction']  = 'in'
-
 pl.rcParams['ytick.major.size'] = 8
 pl.rcParams['ytick.minor.size'] = 4
 pl.rcParams['ytick.major.pad']  = 8
@@ -32,31 +30,16 @@ pl.rcParams['ytick.minor.pad']  = 8
 pl.rcParams['ytick.color']      = 'k'
 pl.rcParams['ytick.labelsize']  = 'medium'
 pl.rcParams['ytick.direction']  = 'in'
+#%%
 
-no_of_particles=[1000,10000,100000,1000000,10000000]
-mean=[0.1445,0.0515,0.01426,0.005068,0.0009538]
-variance=[0.0313806451613,0.00439857548387,0.000365383019355,4.05846874839e-05,1.35727896774e-06]
-std_dev=[0.17714583021141178,0.06632175724353936,0.019114994620842513,0.006370611233144817,0.0011650231618907738]
+no_of_particles = np.array([1000,10000,100000,1000000,10000000])
+error_L1        = np.array([0.1445,0.0515,0.01426,0.005068,0.0009538])
 
+#%%
 pl.figure()
-pl.title('Mean of Error vs No of Particles')
-pl.xlabel('No of particles')
-pl.ylabel('Mean')
-pl.loglog(no_of_particles,mean)
-pl.show()
-
-pl.figure()
-pl.title('Variance of Error vs No of Particles')
-pl.xlabel('No of particles')
-pl.ylabel('Variance')
-pl.loglog(no_of_particles,mean)
-pl.show()
-
-pl.figure()
-pl.title('Standard Deviation of Error vs No of Particles')
-pl.xlabel('No of particles')
-pl.ylabel('Standard Deviation')
-pl.loglog(no_of_particles,mean)
-pl.show()
-
+pl.xlabel('$\mathrm{N}$')
+pl.ylabel('$L_1\;\mathrm{error}$')
+pl.loglog(no_of_particles, error_L1, 'o-')
+pl.loglog(no_of_particles, no_of_particles**-0.5, '--', color='black')
+pl.xlim([100, 1e8])
 
