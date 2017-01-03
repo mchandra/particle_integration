14a1d7af93bf9096023e8e39a2ca232d74cdfebc
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-01-03 23:31:25 +0530

    Made changes to the collision code to account for only 2 body collisions

--- Vectorized/Collisions.py
+++ Vectorized/Collisions.py
@@ -2,12 +2,11 @@ import numpy as np
 import h5py
 from scipy.special import erfinv
 import numexpr as ne
-#import pylab as pl
-ne.set_num_threads(4)
+ne.set_num_threads(8)
 
 """ Setting number of particles and other parameters"""
 
-no_of_particles = 1000
+no_of_particles = 100000
 
 """ Initial conditions """
 
@@ -34,13 +33,14 @@ initial_conditions = np.concatenate([initial_conditions_position_x,initial_condi
 """ Discretizing time and making sure scaling is done right """
 
 box_crossing_time_scale = length_of_box_x/np.max(initial_conditions_velocity_x)
-final_time            = 20*box_crossing_time_scale
-dt   = 0.0001*box_crossing_time_scale
+final_time            = 10*box_crossing_time_scale
+dt   = 0.001*box_crossing_time_scale
 time = np.arange(0, final_time, dt)
 
 """Definition of Verlet Integrator"""
 
 def Verlet(initial_conditions,t):
+
     x=initial_conditions[0:no_of_particles]
     y=initial_conditions[no_of_particles:2*no_of_particles]
     v_x=initial_conditions[2*no_of_particles:3*no_of_particles]
@@ -107,41 +107,52 @@ for time_index,t0 in enumerate(time):
         vely_others=sol[(i+1+3*no_of_particles):4*no_of_particles]
         x_particle=sol[i]
         y_particle=sol[i+no_of_particles]        
-        j=ne.evaluate("j-x_particle")
-        k=ne.evaluate("k-y_particle")
+        j=ne.evaluate("x_particle-j")
+        k=ne.evaluate("y_particle-k")
         dist=ne.evaluate("sqrt(j**2+k**2)")
         j=j/dist
         k=k/dist
         test_collision=ne.evaluate("dist<0.02") 
         test_collision=ne.evaluate("where(test_collision,1,0)")
-        p=(velx*j+vely*k-velx_others*j-vely_others*k)*test_collision
-        velx=velx*test_collision-p*j
-        velx_others=velx_others+p*j
-        vely=vely*test_collision-p*k
-        vely_others=vely_others+p*k 
-        sol[(i+1+2*no_of_particles):3*no_of_particles]=velx_others
-        sol[(i+1+3*no_of_particles):4*no_of_particles]=vely_others
-        if(ne.evaluate("sum(test_collision)")==0):
-            break
+        if(np.sum(test_collision)==0):
+            velx=velx
+        
         else:
-            sol[i+2*no_of_particles]=np.sum(velx)/np.sum(test_collision)
-            sol[i+3*no_of_particles]=np.sum(vely)/np.sum(test_collision)
+            if(np.sum(test_collision)==1):
+                p=(velx*j+vely*k-velx_others*j-vely_others*k)*test_collision
+                velx=velx*test_collision-p*j
+                velx_others=velx_others+p*j
+                vely=vely*test_collision-p*k
+                vely_others=vely_others+p*k 
+                sol[(i+1+2*no_of_particles):3*no_of_particles]=velx_others
+                sol[(i+1+3*no_of_particles):4*no_of_particles]=vely_others
+                sol[i+2*no_of_particles]=np.sum(velx)
+                sol[i+3*no_of_particles]=np.sum(vely)   
+            
+            else:
+
+                for q in range(test_collision.size):
+                    if(test_collision[q]==1):
+                        q=q+1+i
+                        normal=np.array([(sol[i]-sol[q]),(sol[i+no_of_particles]-sol[q+no_of_particles])])
+                        normal=normal/np.linalg.norm(normal)
+                        p=2*(sol[i+2*no_of_particles]*normal[0]+sol[i+3*no_of_particles]*normal[1]-sol[q+2*no_of_particles]*normal[0]\
+                                                   -sol[q+3*no_of_particles]*normal[1])/(2)
+                        sol[i+2*no_of_particles]=sol[i+2*no_of_particles]-p*1*normal[0]
+                        sol[q+2*no_of_particles]=sol[q+2*no_of_particles]+p*1*normal[0]
+                        sol[i+3*no_of_particles]=sol[i+3*no_of_particles]-p*1*normal[1]
+                        sol[q+3*no_of_particles]=sol[q+3*no_of_particles]+p*1*normal[1]
+                        break
+                                                             
 
     old=sol
-    """
-    for i in range(no_of_particles):
-        pl.plot(sol[i],sol[no_of_particles + i], 'o',color='blue', markersize=10, alpha = 0.4)
-        pl.xlim([left_boundary, right_boundary])
-        pl.ylim([bottom_boundary, top_boundary])
-        pl.title('$\mathrm{Time}$ = ' + str(time[time_index]) )
-        pl.xlabel('$x$')
-        pl.ylabel('$y$')
-    print ("Time index = ", time_index)
-    pl.savefig('images/' + '%04d'%time_index + '.png')
-    pl.clf() """
 
     if(time_index==time.size-2):
         fsol=sol    
+    energy=0
+    energy=ne.evaluate("sum(v_x**2+v_y**2)")
+    print("Energy = ",energy)
+
 h5f = h5py.File('post-collision.h5', 'w')
 h5f.create_dataset('sol', data=fsol)
 h5f.close()
