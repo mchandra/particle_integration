d31da53d77eb356054b39112a6185632d2a9eb6c
TejasMane <ezio.tejas@gmail.com>
2017-01-18 12:05:53 +0530

    Update Interpolation.py

--- Interpolation.py
+++ Interpolation.py
@@ -1,4 +1,4 @@
- import numpy as np
+import numpy as np
 import pylab as pl
 import numpy.linalg as la
 
@@ -41,7 +41,7 @@ def getXi(nodal_point, local_domain_nodes):
 def shape(localXi,local_array): 
     
     local_domain_nodes = len(local_array)
-    localXi_matix = [[ 0 ] for i in range(local_domain_nodes)]
+    localXi_matrix = [[ 0 ] for i in range(local_domain_nodes)]
     u = np.matrix(local_array)
     
     for i in range(local_domain_nodes):
@@ -54,12 +54,12 @@ def shape(localXi,local_array):
         for j in range(local_domain_nodes):
             A[i,j] = getXi(i, local_domain_nodes)**j
     
-    c = np.matrix(la.inv(A))*u
+    c = np.matrix(la.inv(A))*u.transpose()
 
-    return np.matrix(localXi_matrix).transpose()*np.matrix(c)
+    return np.matrix(localXi_matrix)*np.matrix(c)
 
 
-Nx = 10
+Nx = 1000
 
 x1 = np.linspace(0, 1, Nx+1)
 x2 = np.linspace(0, 1, Nx+1)
@@ -84,13 +84,22 @@ order_of_interpolation = 2
 
 
 Xi_random_points =   ( (  (random_points*Nx)%(order_of_interpolation)  )/order_of_interpolation  )
-zones_random_points = int(    (random_points*Nx)/order_of_interpolation   )
-
-
-for i in range(len(random_points)):
-    interpolated_electric_fields[i] = shape(Xi_random_points[i],Electric_field[  ( (zones_random_points[i]-1)*(order_of_interpolation+1) -1)   :    ( (zones_random_points[i]-1)*(order_of_interpolation+1) -1)  + order_of_interpolation+1      ] )
+for i in range(number_of_random_points):
+    zones_random_points[i] =     np.int(   (   (random_points[i]*Nx)/order_of_interpolation   )  )
 
 
 
+for i in range(number_of_random_points):
+    interpolated_electric_fields[i] = shape(Xi_random_points[i],Electric_field[  np.int( (zones_random_points[i]-1)*(order_of_interpolation+1) -1)   :     np.int( (zones_random_points[i]-1)*(order_of_interpolation+1) -1)  + order_of_interpolation+1      ] )
+
+
+x_refined = np.linspace(0,1,101)
+E_refined = np.sin(4*np.pi*x_refined)
+
+pl.plot(random_points, interpolated_electric_fields,'*')
+pl.plot(random_points, actual_electric_fields,'o')
+pl.plot(x_refined,E_refined)
+pl.show()
+pl.clf()
 
 
