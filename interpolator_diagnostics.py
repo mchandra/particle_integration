dab01432e802691c4234b89e6900696b19a37e08
TejasMane <ezio.tejas@gmail.com>
2017-01-26 13:33:50 +0530

    Update interpolator_diagnostics.py

--- interpolator_diagnostics.py
+++ interpolator_diagnostics.py
@@ -1,44 +1,4 @@
-import numpy as np
-import pylab as pl
-import numpy.linalg as la
-from Interpolator import bilinear_interpolate
-
-pl.rcParams['figure.figsize'] = 12, 7.5
-pl.rcParams['lines.linewidth'] = 1.5
-pl.rcParams['font.family'] = 'serif'
-pl.rcParams['font.weight'] = 'bold'
-pl.rcParams['font.size'] = 20
-pl.rcParams['font.sans-serif'] = 'serif'
-pl.rcParams['text.usetex'] = True
-pl.rcParams['axes.linewidth'] = 1.5
-pl.rcParams['axes.titlesize'] = 'medium'
-pl.rcParams['axes.labelsize'] = 'medium'
-pl.rcParams['xtick.major.size'] = 8
-pl.rcParams['xtick.minor.size'] = 4
-pl.rcParams['xtick.major.pad'] = 8
-pl.rcParams['xtick.minor.pad'] = 8
-pl.rcParams['xtick.color'] = 'k'
-pl.rcParams['xtick.labelsize'] = 'medium'
-pl.rcParams['xtick.direction'] = 'in'
-pl.rcParams['ytick.major.size'] = 8
-pl.rcParams['ytick.minor.size'] = 4
-pl.rcParams['ytick.major.pad'] = 8
-pl.rcParams['ytick.minor.pad'] = 8
-pl.rcParams['ytick.color'] = 'k'
-pl.rcParams['ytick.labelsize'] = 'medium'
-pl.rcParams['ytick.direction'] = 'in'
-
-""" Number of Ghost cells """
-
-ghost_cells = 1
-
-""" Function for setting initial conditions for the fields """
-
-def initial_fields(x, y):
-  function_value = np.sin(2 * np.pi * x * y) * np.cos(2 * np.pi * x * y)
-
-  return function_value
-
+from params import *
 
 """ Error function """
 
@@ -47,16 +7,16 @@ def error(a):
   print('Computing error for N = ', a)
   """ Getting the two dimension matrix for initializing the fields """
 
-  nx = a  # number of zones not points
-  ny = a  # number of zones not points
+  Nx = a  # number of zones not points
+  Ny = a  # number of zones not points
 
   """ Length of each zone along x and y """
 
-  dx = np.float(1 / (nx))
-  dy = np.float(1 / (ny))
+  dx = np.float(Lx / (Nx))
+  dy = np.float(Ly / (Ny))
 
-  x_center = np.linspace(-dx, 1 + dx, nx + 3, endpoint=True)
-  y_center = np.linspace(-dy, 1 + dy, ny + 3, endpoint=True)
+  x_center = np.linspace(-ghost_cells*dx, 1 + ghost_cells*dx, Nx + 1 + 2 * ghost_cells, endpoint=True)
+  y_center = np.linspace(-ghost_cells*dy, 1 + ghost_cells*dy, Ny + 1 + 2 * ghost_cells, endpoint=True)
 
   """ Initializing the field variables """
 
@@ -64,50 +24,45 @@ def error(a):
   Bx = np.zeros(((len(x_center)), (len(y_center))), dtype=np.float)
   By = np.zeros(((len(x_center)), (len(y_center))), dtype=np.float)
 
-  x_right = np.linspace(-dx / 2, 1 + 3 * dx / 2, nx + 3, endpoint=True)
-  y_top = np.linspace(-dy / 2, 1 + 3 * dy / 2, ny + 3, endpoint=True)
+
+  x_right = np.linspace(-ghost_cells * dx / 2, 1 + (2 * ghost_cells + 1) * dx / 2, Nx + 1 + 2 * ghost_cells,\
+                          endpoint=True\
+                        )
+
+  y_top = np.linspace(-ghost_cells * dy / 2, 1 + (2 * ghost_cells + 1) * dy / 2, Ny + 1 + 2 * ghost_cells,\
+                        endpoint=True\
+                      )
 
   """ Getting the two dimension matrix for initializing the fields """
 
-  X_center_physical, Y_center_physical = np.meshgrid(x_center[ghost_cells:-ghost_cells],\
-                                                      y_center[ghost_cells:-ghost_cells]\
+  X_center_physical, Y_center_physical = np.meshgrid(x_center[ghost_cells:-ghost_cells], \
+                                                      y_center[ghost_cells:-ghost_cells] \
                                                     )
-  
-  X_right_physical, Y_top_physical = np.meshgrid(x_right[ghost_cells:-ghost_cells],\
-                                                 y_top[ghost_cells:-ghost_cells]\
+
+  X_right_physical, Y_top_physical = np.meshgrid(x_right[ghost_cells:-ghost_cells], \
+                                                  y_top[ghost_cells:-ghost_cells] \
                                                 )
 
   """ [-ghostcells:ghostcells] selects the points located in the physical domain excluding the ghost cells """
 
   """ Assigning Field values to the physical physical domain """
-  Ez[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_center_physical,\
+  Ez[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_center_physical, \
                                                                           Y_center_physical
                                                                          )
-  
-  Bx[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_center_physical,\
-                                                                          Y_top_physical\
+
+  Bx[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_center_physical, \
+                                                                          Y_top_physical \
                                                                          )
-  
-  By[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_right_physical,\
-                                                                          Y_center_physical\
+
+  By[ghost_cells:-ghost_cells, ghost_cells:-ghost_cells] = initial_fields(X_right_physical, \
+                                                                          Y_center_physical \
                                                                          )
 
   """ Implementing Periodic Boundary conditions using ghost cells """
 
-  Ez[0, :]                 = Ez[len(y_center) - 1 - ghost_cells, :].copy()
-  Ez[:, 0]                 = Ez[:, len(x_center) - 1 - ghost_cells].copy()
-  Ez[len(y_center) - 1, :] = Ez[ghost_cells, :].copy()
-  Ez[:, len(x_center) - 1] = Ez[:, ghost_cells].copy()
-
-  Bx[0, :]                 = Bx[len(y_top) - 1 - ghost_cells, :].copy()
-  Bx[:, 0]                 = Bx[:, len(x_center) - 1 - ghost_cells].copy()
-  Bx[len(y_top) - 1, :]    = Bx[ghost_cells, :].copy()
-  Bx[:, len(x_center) - 1] = Bx[:, ghost_cells].copy()
-
-  By[0, :]                 = By[len(y_center) - 1 - ghost_cells, :].copy()
-  By[:, 0]                 = By[:, len(x_right) - 1 - ghost_cells].copy()
-  By[len(y_center) - 1, :] = By[ghost_cells, :].copy()
-  By[:, len(x_right) - 1]  = By[:, ghost_cells].copy()
+  Ez = periodic(Ez, len(x_center), len(y_center), ghost_cells )
+  Bx = periodic(Bx, len(x_center), len(y_top), ghost_cells    )
+  By = periodic(By, len(x_right), len(y_center), ghost_cells  )
 
   """ Selecting a number of test points for testing error """
 
@@ -124,17 +79,17 @@ def error(a):
 
   """ Calculating Interpolated values at the randomly selected points """
 
-  Ez_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_center,\
-                                               y_grid=y_center, F=Ez)\
-                                              )
-  
-  Bx_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_center,\
-                                               y_grid=y_top, F=Bx)\
-                                              )
-  
-  By_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_right,\
-                                              y_grid=y_center, F=By)\
-                                              )
+  Ez_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_center, \
+                                                y_grid=y_center, F=Ez), ghost_cells \
+                         )
+
+  Bx_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_center, \
+                                                y_grid=y_top, F=Bx), ghost_cells \
+                          )
+
+  By_at_random = np.array(bilinear_interpolate(x=[x_random], y=[y_random], x_grid=x_right, \
+                                                y_grid=y_center, F=By), ghost_cells \
+                          )
 
   Ez_error = sum(sum(abs(Ez_at_random - [initial_fields(x_random, y_random)]))) / number_random_points
   Bx_error = sum(sum(abs(Bx_at_random - [initial_fields(x_random, y_random)]))) / number_random_points
@@ -151,7 +106,7 @@ error = np.vectorize(error)
 
 # N = np.array([32, 64, 128, 256, 512, 1024])
 
-N = np.arange(100, 4000, 100)
+N = np.arange(100, 2000, 50)
 
 """ Computing error at the corresponding grid densities """
 
