18a00223c86c0b3f3ab252a731951d781ab3157e
TejasMane <ezio.tejas@gmail.com>
2017-02-05 00:17:22 +0530

    Update currentdepositer.py

--- currentdepositer.py
+++ currentdepositer.py
@@ -1,3 +1,96 @@
+from params import *
+import numpy as np
 # Returns current at n+1/2 when x_n and x_n+1 are provided
 
-def dcd(positions_n,positions_n_plus_one ,velocites_n_plus_half,x_center_grid, y_center_grid, shape_function):
+# Ex = (x_right, y_center )
+# Ey = (x_center, y_top   )
+# Ez = (x_center, y_center)
+# Bx = (x_center, y_top   )
+# By = (x_right, y_center )
+# Bz = (x_right, y_top    )
+# rho = (x_center, y_top ) # Not needed here
+# Jx = (x_right, y_center )
+# Jy = (x_center, y_top )
+# Jz = (x_center, y_center )
+
+"""Charge Deposition for B0 splines (Have to vectorize)"""
+
+
+
+# def charge_b0_depositor(xi, yi, 2d_grid, dx):
+#     charged_grid = np.zeros((len(x_grid), len(y_grid)),dtype = np.float)
+#     for particle_index in len(xi):
+#         for x_index in len(x_grid):
+#             for y_index in len(y_grid):
+#                 if(abs((xi[particle_index]-x_grid[x_index])/dx<0.5) and abs((yi[particle_index]-y_grid[y_index])/dy<0.5) ):
+#                     charged_grid[x_index, y_index] +=1
+#
+#     return charged_grid
+
+
+
+"""Current Deposition for B0 splines (Have to vectorize)"""
+
+def current_b0_depositor(x, y, velocity_required, x_grid, y_grid, J, ghost_cells, Lx, Ly):
+
+
+    n = (len(x_grid) - 1 - 2 * ghost_cells)  # number of zones
+
+    dx = Lx/n
+    dy = Ly/n
+    print('should be printed once')
+    x_zone = int(n * np.float(x - x_grid[0])/Lx)  # indexing from zero itself
+    y_zone = int(n * np.float(y - y_grid[0])/Ly)
+
+    if(abs(x-x_grid[x_zone])<abs(x-x_grid[x_zone + 1])):
+        x_current_zone = x_zone
+    else:
+        x_current_zone = x_zone +1
+
+
+    if(abs(y - y_grid[y_zone])<abs(y - y_grid[y_zone + 1])):
+        y_current_zone = y_zone
+    else:
+        y_current_zone = y_zone +1
+
+    # J[y_current_zone, x_current_zone]+= (charge/(dx*dy))*velocity_required
+    # return J
+    return y_current_zone,x_current_zone,((charge/(dx*dy))*velocity_required)
+
+
+current_b0_depositor = np.vectorize(current_b0_depositor, excluded=(['x_grid', 'y_grid', 'J','ghost_cells', 'Lx', 'Ly']))
+
+x = np.array([0.2, 0.6])
+y = np.array([0.2, 0.6])
+v = np.array([1, 1])
+
+x_grid = np.array([-0.5, 0, 0.5, 1, 1.5])
+y_grid = np.array([-0.5, 0, 0.5, 1, 1.5])
+
+J = np.matrix('0 0 0 0 0;0 0 0 0 0;0 0 0 0 0;0 0 0 0 0')
+
+J = np.array(current_b0_depositor(x = [x], y= [y], velocity_required = [v], x_grid = x_grid, y_grid = y_grid, J = J, ghost_cells = ghost_cells, Lx = Lx, Ly = Ly   ))
+
+print(J)
+
+
+# J = current_b0_depositor(x = x, y= y, velocity_required = v, x_grid = x_grid, y_grid = y_grid,\
+#                            J = J, ghost_cells = ghost_cells, Lx = Lx, Ly = Ly \
+#                            )\
+#
+# print(J)
+
+
+
+# def dcd(positions_n_plus_half ,velocites_n_plus_half, x_center_grid, y_center_grid,shape_function, no_of_particles, dx, dy):
+#
+#     x_right_grid = x_center_grid + dx
+#     y_top_grid = y_center_grid + dy
+#
+#
+#
+#     Jx = shape_function(positions_n_plus_half[:no_of_particles], velocites_n_plus_half[:no_of_particles], x_right_grid, y_center_grid, dx, dy)
+#     Jy = shape_function(positions_n_plus_half[no_of_particles:2*no_of_particles], velocites_n_plus_half[no_of_particles:2*no_of_particles], x_center_grid, y_top_grid, dx, dy)
+#     Jz = shape_function(positions_n_plus_half[2*no_of_particles:3*no_of_particles], velocites_n_plus_half[2*no_of_particles:3*no_of_particles], x_center_grid, y_center_grid, dx, dy)
+#
+#     return Jx, Jy, Jz
