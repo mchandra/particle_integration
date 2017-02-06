50eac0386f5c29b203ea4bc1cc1c34138846ecd3
TejasMane <ezio.tejas@gmail.com>
2017-02-06 10:45:30 +0530

    Update currentdepositer.py

--- currentdepositer.py
+++ currentdepositer.py
@@ -23,26 +23,26 @@ from fields import *
 def charge_b0_depositor(x, y, x_grid, y_grid, J, ghost_cells, Lx, Ly):
 
 
-    n = (len(x_grid) - 1 - 2 * ghost_cells)  # number of zones
+  n = (len(x_grid) - 1 - 2 * ghost_cells)  # number of zones
 
-    dx = Lx/n
-    dy = Ly/n
+  dx = Lx/n
+  dy = Ly/n
 
-    x_zone = int(n * np.float(x - x_grid[0])/Lx)  # indexing from zero itself
-    y_zone = int(n * np.float(y - y_grid[0])/Ly)
+  x_zone = int(n * np.float(x - x_grid[0])/Lx)  # indexing from zero itself
+  y_zone = int(n * np.float(y - y_grid[0])/Ly)
 
-    if(abs(x-x_grid[x_zone])<abs(x-x_grid[x_zone + 1])):
-        x_current_zone = x_zone
-    else:
-        x_current_zone = x_zone +1
+  if(abs(x-x_grid[x_zone])<abs(x-x_grid[x_zone + 1])):
+    x_current_zone = x_zone
+  else:
+    x_current_zone = x_zone +1
 
 
-    if(abs(y - y_grid[y_zone])<abs(y - y_grid[y_zone + 1])):
-        y_current_zone = y_zone
-    else:
-        y_current_zone = y_zone +1
+  if(abs(y - y_grid[y_zone])<abs(y - y_grid[y_zone + 1])):
+    y_current_zone = y_zone
+  else:
+    y_current_zone = y_zone +1
 
-    return y_current_zone,x_current_zone,((charge/(dx*dy)))
+  return y_current_zone,x_current_zone,((charge/(dx*dy)))
 
 
 charge_b0_depositor = np.vectorize(charge_b0_depositor, excluded=(['x_grid', 'y_grid', 'J','ghost_cells', 'Lx', 'Ly']))
@@ -71,38 +71,24 @@ charge_b0_depositor = np.vectorize(charge_b0_depositor, excluded=(['x_grid', 'y_
 def current_b0_depositor(charge, x, y, velocity_required, x_grid, y_grid, ghost_cells, Lx, Ly):
 
 
-    n = (len(x_grid) - 1 - 2 * ghost_cells)  # number of zones
+  n = (len(x_grid) - 1 - 2 * ghost_cells)  # number of zones
 
-    dx = Lx/n
-    dy = Ly/n
-    x_zone = int(n * np.float(x - x_grid[0])/Lx)  # indexing from zero itself
-    y_zone = int(n * np.float(y - y_grid[0])/Ly)
+  dx = Lx/n
+  dy = Ly/n
+  x_zone = int(n * np.float(x - x_grid[0])/Lx)  # indexing from zero itself
+  y_zone = int(n * np.float(y - y_grid[0])/Ly)
 
-    if(abs(x-x_grid[x_zone])<abs(x-x_grid[x_zone + 1])):
-        x_current_zone = x_zone
-    else:
-        x_current_zone = x_zone +1
+  if(abs(x-x_grid[x_zone])<abs(x-x_grid[x_zone + 1])):
+    x_current_zone = x_zone
+  else:
+    x_current_zone = x_zone +1
 
-    print('x_grid',len(x_grid))
-    print('x', x)
-    print('x_zone', x_zone)
+  if(abs(y - y_grid[y_zone])<abs(y - y_grid[y_zone + 1])):
+    y_current_zone = y_zone
+  else:
+    y_current_zone = y_zone +1
 
-    print('\n\n\n\n\n')
-    #fasssssss
-
-    print('y_grid',len(y_grid))
-    print('y', y)
-    print('y_zone', y_zone)
-
-
-
-
-    if(abs(y - y_grid[y_zone])<abs(y - y_grid[y_zone + 1])):
-        y_current_zone = y_zone
-    else:
-        y_current_zone = y_zone +1
-
-    return y_current_zone,x_current_zone,((charge/(dx*dy))*velocity_required)
+  return y_current_zone,x_current_zone,((charge/(dx*dy))*velocity_required)
 
 
 current_b0_depositor = np.vectorize(current_b0_depositor, excluded=(['charge','x_grid', 'y_grid', 'ghost_cells', 'Lx', 'Ly']))
@@ -125,7 +111,7 @@ current_b0_depositor = np.vectorize(current_b0_depositor, excluded=(['charge','x
 #
 # print('The i shape ', i.shape)
 # print('The i  first element ', i[0,0])
-#
+
 
 
 # def fun(f):
@@ -148,57 +134,49 @@ current_b0_depositor = np.vectorize(current_b0_depositor, excluded=(['charge','x
 
 def dcd(charge, no_of_particles, positions_plus_half ,velocities_plus_half, x_center_grid, y_center_grid,shape_function, ghost_cells, Lx, Ly, dx, dy):
 
-    x_right_grid = x_center_grid + dx
-    y_top_grid = y_center_grid + dy
+  x_right_grid = x_center_grid + dx
+  y_top_grid = y_center_grid + dy
 
-    positions_x = positions_plus_half[:no_of_particles]
-    positions_y = positions_plus_half[no_of_particles:2*no_of_particles]
+  positions_x = positions_plus_half[:no_of_particles]
+  positions_y = positions_plus_half[no_of_particles:2*no_of_particles]
 
-    velocities_x = velocities_plus_half[:no_of_particles]
-    velocities_y = velocities_plus_half[no_of_particles:2*no_of_particles]
-    velocities_z = velocities_plus_half[2*no_of_particles:3*no_of_particles]
-
-
-    Jx = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
-    Jy = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
-    Jz = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
-
-    # print('fasfasfasfffff')
+  velocities_x = velocities_plus_half[:no_of_particles]
+  velocities_y = velocities_plus_half[no_of_particles:2*no_of_particles]
+  velocities_z = velocities_plus_half[2*no_of_particles:3*no_of_particles]
 
 
-
-    # print('number of particle = ', no_of_particles)
-    # print('length of positions given  ',len(positions_plus_half))
-    # print('x',len(positions_x))
-    # print('y',len(positions_y))
-    # print('vx',len(velocities_x))
-    print('y inside dcd function',positions_y)
-    # print(shape_function(charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_x],x_grid = x_right_grid, y_grid = y_center_grid,\
-    #                                                                         ghost_cells = ghost_cells,Lx =Lx, Ly= Ly))
-
-    # print('Blashhhhhhhhhhhhh')
-
-    Jx_x_indice, Jx_y_indices, Jx_values_at_these_indices = shape_function(charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_x],x_grid = x_right_grid, y_grid = y_center_grid,\
-                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly)
+  Jx = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
+  Jy = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
+  Jz = np.zeros((len(x_center_grid), len(y_center_grid)), dtype=np.float)
 
 
-    for i in range(no_of_particles):
-        Jx[Jx_x_indice[0,i], Jx_y_indices[0,i]] = Jx_values_at_these_indices[0,i]
+  Jx_x_indice, Jx_y_indices, Jx_values_at_these_indices = shape_function(   charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_x],\
+                                                                            x_grid = x_right_grid, y_grid = y_center_grid,\
+                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly\
+                                                                        )
 
-    Jy_x_indice, Jy_y_indices, Jy_values_at_these_indices = shape_function(charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_y],x_grid = x_center_grid, y_grid = y_top_grid,\
-                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly)
+
+  for i in range(no_of_particles):
+    Jx[Jx_x_indice[0,i], Jx_y_indices[0,i]] = Jx_values_at_these_indices[0,i]
+
+  Jy_x_indice, Jy_y_indices, Jy_values_at_these_indices = shape_function(   charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_y],\
+                                                                            x_grid = x_center_grid, y_grid = y_top_grid,\
+                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly\
+                                                                        )
 
 
-    for i in range(no_of_particles):
-        Jy[Jy_x_indice[0,i], Jy_y_indices[0,i]] = Jy_values_at_these_indices[0,i]
+  for i in range(no_of_particles):
+    Jy[Jy_x_indice[0,i], Jy_y_indices[0,i]] = Jy_values_at_these_indices[0,i]
 
 
 
-    Jz_x_indice, Jz_y_indices, Jz_values_at_these_indices = shape_function(charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_z],x_grid = x_center_grid, y_grid = y_center_grid,\
-                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly)
+  Jz_x_indice, Jz_y_indices, Jz_values_at_these_indices = shape_function(   charge = charge,x = [positions_x], y = [positions_y], velocity_required = [velocities_z],\
+                                                                            x_grid = x_center_grid, y_grid = y_center_grid,\
+                                                                            ghost_cells = ghost_cells,Lx =Lx, Ly= Ly\
+                                                                        )
 
 
-    for i in range(no_of_particles):
-        Jy[Jz_x_indice[0,i], Jz_y_indices[0,i]] = Jz_values_at_these_indices[0,i]
+  for i in range(no_of_particles):
+    Jy[Jz_x_indice[0,i], Jz_y_indices[0,i]] = Jz_values_at_these_indices[0,i]
 
-    return Jx, Jy, Jz
+  return Jx, Jy, Jz
