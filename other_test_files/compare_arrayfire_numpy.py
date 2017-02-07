43695f6ba00800008cece7650a66b4da6c9fdfb9
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-02-07 13:48:24 +0530

    Changed the benchmark test file

--- other_test_files/compare_arrayfire_numpy.py
+++ other_test_files/compare_arrayfire_numpy.py
@@ -5,26 +5,35 @@ af.set_backend("opencl")
 
 # This is a benchmark test to compare numpy and arrayfire:
 
+aNumPy = np.random.rand(100, 100)
+bNumPy = np.random.rand(100, 100)
+
 np_time_start = time.time()
 
-a = np.array([1, 2, 3, 4, 5])
-b = np.array([6, 7, 8, 9, 10])
-c = a + b
+for i in range(1e6):
+  cNumPy = aNumPy + bNumPy
 
 np_time_end     = time.time()
 np_time_elapsed = np_time_end - np_time_start
 
-print("numpy answer is = ", c)
+#print("numpy answer is = ", cNumPy)
 print("numpy implementation run took time =", np_time_elapsed," seconds")
+
+aArrayFire = af.Array(aNumPy.ctypes.data, aNumPy.shape, aNumPy.dtype.char)
+bArrayFire = af.Array(bNumPy.ctypes.data, bNumPy.shape, bNumPy.dtype.char)
+cArrayFire = aArrayFire + bArrayFire
+cArrayFire.eval()
+af.sync()
 
 af_time_start = time.time()
 
-a = af.Array([1, 2, 3, 4, 5])
-b = af.Array([6, 7, 8, 9, 10])
-c = a + b
+for i in range(1e6):
+  cArrayFire = aArrayFire + bArrayFire
+  cArrayFire.eval()
 
+af.sync()
 af_time_end     = time.time()
 af_time_elapsed = af_time_end - af_time_start
 
-print("arrayfire answer is = ", c)
+#print("arrayfire answer is = ", cArrayFire)
 print("arrayfire implementation run took time =", af_time_elapsed," seconds")
